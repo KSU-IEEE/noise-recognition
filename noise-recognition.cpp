83d7e2fe87d4da47fa761c71a235c46a71d7ee54
jhalimro <60119007+jhalimro@users.noreply.github.com>
2021-02-24 20:43:18 -0500

    Add files via upload
    Current progress

--- noise-recognition.cpp
+++ noise-recognition.cpp
@@ -1,15 +1,17 @@
 #include "noise-recognition.h"
 #include <arduinoFFT.h>
+#include <math.h> 
 namespace noise_recognition{
 
+double vReal[SAMPLES_]; //create vector of size SAMPLES to hold real values
+double vImag[SAMPLES_]; //create vector of size SAMPLES to hold imaginary values
 
 /*
 useage:
-
 noiseRecognition nr;
 nh.listen(); // 1.) get_sample() 2.) performFFT() 3.) hasFreq() <- return hasFreq()
-*/
 
+Commented out stuff:
 
 noiseRecognition::noiseRecognition(double freq){
     /*if(freq >= threshHoldValue)
@@ -18,30 +20,55 @@ noiseRecognition::noiseRecognition(double freq){
     }
     else{
     return false
+    }
     }*/
-}
 
-void setFreq(double val); 
-bool listen(); 
 
-bool noiseRecognition::hasFreq() {
+//void setFreq(double val); 
+
+
+
+bool listen()
+{
+arduinoFFT samples = getSamples();
+bool existingFreq = hasFreq(samples);
+return existingFreq;
+} 
+
+bool noiseRecognition::hasFreq(arduinoFFT FFT) {
     // line 65
     // check if double peak is within a tolerance (class variable)
+     double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+     if (peak > 0)
+     {
+         return true;
+     }
+     else{
+        return false;
+     }
 }
 
-void noiseRecognition::performFFT(){
+//void noiseRecognition::performFFT(){
     //FFT must be global variable
     //Need to find a way to iterate
-    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
-    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+    
+    
 }
 
 // return vector
 void noiseRecognition::getSamples(){
 //set global FFT variable to value obtained from getSample
 // lines 45 - 57
-
+//getting fft value
+arduinoFFT FFT = arduinoFFT();
+for (int i = 0; i < sizeOf(vReal); ++i)
+    {
+    vReal[i] = analogRead(0);
+    vImag[i] = 0;
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+    }
 /*
 wave = r * cos(T + phi) + i * sin(T + phi)
             i
@@ -56,7 +83,6 @@ wave = r * cos(T + phi) + i * sin(T + phi)
             |
 Sample - array of i and array of r 
 Sample Frequency - how often you save i and r 
-
 Sample frequency = 20,000
 const int FS_HZ = 20000;  // samping frequency
 const int SAMP_PERIOD_US = (1 / FS_HZ) * 10000000// sampling period
